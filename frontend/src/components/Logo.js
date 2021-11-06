48f1264c4b70c7b4947f180e2488d42730d846a0
Sam Burkhard <samryanburk@icloud.com>
2021-11-06 15:27:10 -0400

    updated logo reference

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -4,7 +4,7 @@ import '../css/logo.css';
 const Logo = function() {
     return (
         <div class="logo-svg">
-            <img src="/InSite-Logo.svg"/>
+            <img src="/logo.svg"/>
             <text className="text-wrapper">truth matters</text>
         </div>   
     )
