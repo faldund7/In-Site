569ef6e53ba08246fb12c6498cb3eaa7aa5e4747
Zenith Zack <faldund@outlook.com>
2021-11-06 18:33:00 -0400

    function for submit button

--- frontend/src/components/TextField.js
+++ frontend/src/components/TextField.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 
 const TextField = function() {
+    const [text, setText] = useState("");
+
+    const handleSubmit = function(event){
+        event.preventDefault();
+        alert('Pressed Submit!')
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
-                <input type="text" name="query" class="input-field" placeholder="Enter a sentence"/>
+                <input type="text" name="query" class="input-field" placeholder="Enter a sentence" onChange={e => setText(e.target.value)}/>
             </label>
             <input type="submit" name="submit" className="style-button"/>
         </form>
