ac810d01fb4d4edfda45275d35dd670ed1c33767
Zenith Zack <faldund@outlook.com>
2021-11-06 20:53:06 -0400

    preliminary pie-chart

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,11 +1,17 @@
 import styled, { css } from 'styled-components';
 import Chart from 'react-apexcharts';
+import { useState } from 'react';
 
 const Card = styled.div`
-    height: 10rem;
+    height: 20rem;
     width: 25rem;
     border-radius: 1rem;
     background: #000000;
+`
+const ChartDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
 const CardTitle = styled.div`
@@ -17,16 +23,54 @@ const CardTitle = styled.div`
 `
 
 const CardContent = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     font-size: 12px;
     color: white;
 `
 
+
 const SimpleCard = function(){
+    const [options, setOptions] = useState({
+        chart: {
+            animations: {
+              enabled: false,
+            },
+          },
+          grid: {
+            padding: {
+              top: 20,
+              left: 0,
+              right: 0,
+              bottom: 20,
+            },
+          },
+          stroke: {
+            show: false,
+          },
+          tooltip: { enabled: false },
+          legend: { show: false },
+          dataLabels: { enabled: false },
+          plotOptions: { pie: { donut: { size: "100%" } } },
+    
+    });
+
+    const series = [20, 50, 30];
+
     return(
         <Card>
             <CardTitle>Simple Card</CardTitle>
             <CardContent>Simple Card</CardContent>
-            {/* <Chart></Chart> */}
+            <ChartDiv>
+                <Chart options={options}
+                series={series}
+                type="pie"
+                width="100%"
+                height="auto"
+                >
+                </Chart>
+            </ChartDiv>
         </Card>
     );
 }
