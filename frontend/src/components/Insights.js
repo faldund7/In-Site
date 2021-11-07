8cbdb09147a4aa0c303ec2f7cb4a64fea543cd5c
Zenith Zack <faldund@outlook.com>
2021-11-07 00:13:50 -0400

    dashboard milestone

--- frontend/src/components/Insights.js
+++ frontend/src/components/Insights.js
@@ -2,14 +2,14 @@ import styled, { css } from 'styled-components';
 
 const ArticleCard = styled.div`
     height: 200px;
-    width: 200px;
+    width: 300px;
     border-radius: 1rem;
     background: #053868;
 `
 
 const TextCard = styled.div`
     height: 200px;
-    width: 200px;
+    width: 300px;
     border-radius: 1rem;
     background: #053868;
 `
@@ -28,6 +28,28 @@ const TextCardContent = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    padding-left: 30px;
+    padding-right: 30px;
+    font-size: 10px;
+    color: black;
+`
+
+const ArticleCardTitle = styled.div`
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #ffffff;
+    font-size: 18px;
+`
+
+const ArticleCardContent = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-left: 30px;
+    padding-right: 30px;
     font-size: 10px;
     color: black;
 `
@@ -51,7 +73,13 @@ const Insights = function(){
                     as opposed to using 'Content here, content here', making it look like readable English.
                 </TextCardContent>
             </TextCard>
-            <ArticleCard></ArticleCard>
+            <ArticleCard>
+                <ArticleCardTitle>Why Cheese is out for your money</ArticleCardTitle>
+                <ArticleCardContent>The cheese mafia will murder your family and steal everything from you. They hate you and your non-cheese ways. 
+                    I hope you die in a bucket of cheese where you belong. It’s delicous. I envy you and your cheese bucket death. 
+                    Some of us don’t get to die a hero. But you, you will die in a cheese bucket.
+                </ArticleCardContent>
+            </ArticleCard>
         </div>
         <div className="box-wrapper">
             <TextCard>
@@ -61,7 +89,13 @@ const Insights = function(){
                     as opposed to using 'Content here, content here', making it look like readable English.
                 </TextCardContent>
             </TextCard>
-            <ArticleCard></ArticleCard>
+            <ArticleCard>
+                <ArticleCardTitle>Why Cheese is out for your money</ArticleCardTitle>
+                <ArticleCardContent>The cheese mafia will murder your family and steal everything from you. They hate you and your non-cheese ways. 
+                    I hope you die in a bucket of cheese where you belong. It’s delicous. I envy you and your cheese bucket death. 
+                    Some of us don’t get to die a hero. But you, you will die in a cheese bucket.
+                </ArticleCardContent>
+            </ArticleCard>
         </div>
     </div>
         
